1277fef0003c85ccddf6eb602c362a5275f8db57
Erik Rodriguez <eric19.21.03@gmail.com>
2020-06-19 12:48:03 -0700

    Adds count_th function to count th file

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,15 +4,22 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 
+# set up a list track th
+th_count = []
+
 
 def count_th(word):
-    if word is "":
-        print("nioce empty")
+    # empty string, so return count
+    if word == "":
+        return len(th_count)
 
-    # if "th" in word:
-
-    # empty word could be base case
-
-    # set up a counter to track th
-
-    # return counter of th
+    # if "th" in word, then remove them
+    if "th" in word:
+        th_count.append(0)
+        new_word = word.replace("th", "0", 1)
+        return count_th(new_word)
+    # else return the count and clear list
+    else:
+        value = len(th_count)
+        th_count.clear()
+        return value
